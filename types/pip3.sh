cbed9de1f7e2ddf123633a57db69815aa132cad2
Skylar MacDonald <skylar@skylarmacdonald.com>
2021-02-09 23:31:44 +0000

    Add type for pip3

---
+++ types/pip3.sh
@@ -0,0 +1,25 @@
+# TODO --sudo flag
+# TODO versions
+# TODO update
+
+action=$1
+name=$2
+shift 2
+
+case $action in
+  desc)
+    echo "asserts presence of packages installed via pip"
+    echo "> pip3 pygments"
+    ;;
+  status)
+    needs_exec "pip3" || return $STATUS_FAILED_PRECONDITION
+    pkgs=$(PIP_FORMAT=legacy bake pip3 list)
+    if ! str_matches "$pkgs" "^$name"; then
+      return $STATUS_MISSING
+    fi
+    return 0 ;;
+  install)
+    bake pip3 install "$name"
+    ;;
+esac
+
