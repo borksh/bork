88874509af4493c416454db3dd7078ff9fa2301d
Skylar MacDonald <biology-loose0g@icloud.com>
2022-03-16 16:59:56 +0000

    Improve fpm config for macOS pkg

--- pkg/after-install.sh
+++ pkg/after-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if ! [[ -e "/usr/local/bin/bork" ]]; then
+if ! [ -d "/usr/local/bin" ]; then
+  mkdir /usr/local/bin
+fi
+if ! [ -e "/usr/local/bin/bork" ]; then
   ln -s /usr/local/src/bork/bin/bork /usr/local/bin/bork
 fi
--- pkg/after-uninstall.sh
+++ pkg/after-uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ -L "/usr/local/bin/bork" ]]; then
+if [ -L "/usr/local/bin/bork" ]; then
   rm /usr/local/bin/bork
 fi
