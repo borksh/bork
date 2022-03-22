cd0003c752c1a3d91c2cb5c43e15ca48d27ed0d9
Skylar MacDonald <biology-loose0g@icloud.com>
2022-03-22 22:34:15 +0000

    Complete bork types and 'ok' assertion

--- pkg/bash_completions.sh
+++ pkg/bash_completions.sh
@@ -1,23 +1,52 @@
 #/usr/bin/env bash
 
+_bork_typelist () {
+  BORK_SOURCE_DIR=$(dirname $(dirname $(realpath $(which bork))))
+  typefiles=$()
+  BORK_TYPES=""
+  for type in $BORK_SOURCE_DIR/types/*.sh; do
+    BORK_TYPES="$BORK_TYPES $(basename $type '.sh')"
+  done
+}
+
 _bork_completions () {
   if [ "${#COMP_WORDS[@]}" -le "2" ]; then
     COMPREPLY=($(compgen -W "check compile do satisfy status inspect types docgen version" "${COMP_WORDS[1]}"))
   elif [ "${#COMP_WORDS[@]}" -gt "2" ]; then
-    local IFS=$'\n'
-    local LASTCHAR=' '
+    case ${COMP_WORDS[1]} in
+      status|satisfy|compile)
+        local IFS=$'\n'
+        local LASTCHAR=' '
 
-    COMPREPLY=($(compgen -o plusdirs -f \
-        -- "${COMP_WORDS[COMP_CWORD]}"))
+        COMPREPLY=($(compgen -o plusdirs -f \
+          -- "${COMP_WORDS[COMP_CWORD]}"))
 
-    if [ ${#COMPREPLY[@]} = 1 ]; then
-        [ -d "$COMPREPLY" ] && LASTCHAR=/
-        COMPREPLY=$(printf %q%s "$COMPREPLY" "$LASTCHAR")
-    else
-        for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+        if [ ${#COMPREPLY[@]} = 1 ]; then
+          [ -d "$COMPREPLY" ] && LASTCHAR=/
+          COMPREPLY=$(printf %q%s "$COMPREPLY" "$LASTCHAR")
+        else
+          for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
             [ -d "${COMPREPLY[$i]}" ] && COMPREPLY[$i]=${COMPREPLY[$i]}/
-        done
-    fi
+          done
+        fi
+        ;;
+      check|do)
+        if [ "${#COMP_WORDS[@]}" == "3" ]; then
+          COMPREPLY='ok'
+        elif [ "${#COMP_WORDS[@]}" == "4" ]; then
+          _bork_typelist
+          COMPREPLY=$(compgen -W "$BORK_TYPES" "${COMP_WORDS[3]}")
+        else
+          return
+        fi ;;
+      types|inspect)
+        _bork_typelist
+        COMPREPLY=$(compgen -W "$BORK_TYPES" "${COMP_WORDS[2]}") ;;
+      docgen|version)
+        return ;;
+      *)
+        return ;;
+    esac
   fi
 }
 
--- pkg/zsh_completions.sh
+++ pkg/zsh_completions.sh
@@ -1,8 +1,37 @@
 #compdef bork
 
+_bork_typelist () {
+  BORK_SOURCE_DIR=$(dirname $(dirname $(realpath $(which bork))))
+  typefiles=$()
+  BORK_TYPES=()
+  for type in $BORK_SOURCE_DIR/types/*.sh; do
+    BORK_TYPES+=$(basename $type '.sh')
+  done
+}
+
 _bork () {
   if [ "${#words[@]}" -gt "2" ]; then
-    _files
+    case ${words[2]} in
+      status|satisfy|compile)
+        _files ;;
+      check|do)
+        if [ "${#words[@]}" = "3" ]; then
+          assertions=("ok")
+          _describe 'bork' assertions
+        elif [ "${#words[@]}" = "4" ]; then
+          _bork_typelist
+          _describe 'bork' BORK_TYPES
+        else
+          return
+        fi ;;
+      types|inspect)
+        _bork_typelist
+        _describe 'bork' BORK_TYPES ;;
+      docgen|version)
+        return ;;
+      *)
+        return ;;
+    esac
   else
     local -a borkcmds
     borkcmds=(
