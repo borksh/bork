5bb6c631e44d308225afeffaf61cd09068b65eb8
Skylar MacDonald <biology-loose0g@icloud.com>
2022-03-22 19:15:33 +0000

    Complete files after the first argument

--- pkg/bash_completions.sh
+++ pkg/bash_completions.sh
@@ -1,10 +1,24 @@
 #/usr/bin/env bash
 
 _bork_completions () {
-  if [ "${#COMP_WORDS[@]}" -gt "1" ]; then
-    return
+  if [ "${#COMP_WORDS[@]}" -le "2" ]; then
+    COMPREPLY=($(compgen -W "check compile do satisfy status inspect types docgen version" "${COMP_WORDS[1]}"))
+  elif [ "${#COMP_WORDS[@]}" -gt "2" ]; then
+    local IFS=$'\n'
+    local LASTCHAR=' '
+
+    COMPREPLY=($(compgen -o plusdirs -f \
+        -- "${COMP_WORDS[COMP_CWORD]}"))
+
+    if [ ${#COMPREPLY[@]} = 1 ]; then
+        [ -d "$COMPREPLY" ] && LASTCHAR=/
+        COMPREPLY=$(printf %q%s "$COMPREPLY" "$LASTCHAR")
+    else
+        for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+            [ -d "${COMPREPLY[$i]}" ] && COMPREPLY[$i]=${COMPREPLY[$i]}/
+        done
+    fi
   fi
-  COMPREPLY=($(compgen -W "check compile do satisfy status inspect types docgen version" "${COMP_WORDS[1]}"))
 }
 
-complete -F _bork_completions bork
+complete -o nospace -F _bork_completions bork
