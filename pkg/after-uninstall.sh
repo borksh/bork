637b79e7ad7b9caf3d49361a6bfc3d44840896a6
Skylar MacDonald <biology-loose0g@icloud.com>
2022-03-23 15:29:02 +0000

    Install completions with packages

--- pkg/after-uninstall.sh
+++ pkg/after-uninstall.sh
@@ -3,3 +3,19 @@
 if [ -L "/usr/local/bin/bork" ]; then
   rm /usr/local/bin/bork
 fi
+
+## Shell completions
+
+if [ -L "/usr/share/bash-completion/completions/bork" ]; then
+  rm /usr/share/bash-completion/completions/bork
+fi
+install_platform=$(uname -s)
+if [ $install_platform = "Darwin" ]; then
+  if [ -L "/usr/local/share/zsh/site-functions/_bork" ]; then
+    rm /usr/local/share/zsh/site-functions/_bork
+  fi
+else
+  if [ -L "/usr/share/zsh/site-functions/_bork" ]; then
+    rm /usr/share/zsh/site-functions/_bork
+  fi
+fi
